80a9da49b0f15d9f62609cfb643b88b814932506
Quoc Hoang <hoangvanquoc923@gmail.com>
2024-03-26 16:52:14 +0700

    add needed dependencies, create packages

---
+++ src/main/java/com/hoangquoc/mydaisyhotel/MyDaisyHotelApplication.java
@@ -0,0 +1,13 @@
+package com.hoangquoc.mydaisyhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDaisyHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDaisyHotelApplication.class, args);
+    }
+
+}
